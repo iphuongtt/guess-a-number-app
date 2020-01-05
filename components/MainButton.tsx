87097286aa701cd1ae942478d5bad6afc936e249
phuongtt <iphuongtt@gmail.com>
2020-01-06 01:12:02 +0700

    101. Using Platform-specific code files

--- components/MainButton.tsx
+++ components/MainButton.tsx
@@ -1,52 +1,7 @@
-import React, {ReactNode} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-  Platform,
-  TouchableOpacityProps,
-} from 'react-native';
-import {colors} from '../constants';
+import ios from './MainButton.ios';
+import android from './MainButton.android';
 
-interface Props extends TouchableOpacityProps {
-  children: ReactNode;
-  title?: string;
-}
+declare var _test: typeof ios; // eslint-disable-line no-var
+declare var _test: typeof android; // eslint-disable-line no-var
 
-export const MainButton = (props: Props) => {
-  let ButtonComponent: ReactNode = TouchableOpacity;
-  if (Platform.OS === 'android' && Platform.Version >= 21) {
-    ButtonComponent = TouchableNativeFeedback;
-  }
-  return (
-    <View style={styles.buttonContainer}>
-      <ButtonComponent onPress={props.onPress} activeOpacity={0.8}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>{props.children}</Text>
-        </View>
-      </ButtonComponent>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  buttonContainer: {
-    borderRadius: 25,
-    overflow: 'hidden',
-  },
-  button: {
-    backgroundColor: colors.primary,
-    paddingVertical: 12,
-    paddingHorizontal: 30,
-    borderRadius: 25,
-  },
-  buttonText: {
-    color: 'white',
-    fontFamily: 'OpenSans-Regular',
-    fontSize: 18,
-  },
-});
-
-export default MainButton;
+export {ios as MainButton};
